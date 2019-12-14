2b0e73a28ad8f4085bd79b4171f0d82fad363b38
Johannes Jeising <Johannes.Jeising@gmail.com>
2019-12-14 13:39:31 +0100

    add libcurt

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate neon;
+extern crate libcurt;
 
 use neon::prelude::*;
 
@@ -7,6 +8,4 @@ fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
 }
 
-register_module!(mut cx, {
-    cx.export_function("hello", hello)
-});
+register_module!(mut cx, { cx.export_function("hello", hello) });
